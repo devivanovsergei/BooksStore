dba83fa92a2aec5b487c4892eac0144bc4ccdbaf
Сергей <devivanovsergey@gmail.com>
2017-01-30 22:44:47 +0400

    StartCode

---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book { Name="Язык программирования C# 5.0 и платформа .NET 4.5",Author="Троелсен Э.", Price=1179},
+                new Book { Name="C# 5.0 и платформа .NET 4.5 для профессионалов",Author="Карли Уотсон и др.", Price=1155},
+                new Book { Name="Асинхронное программирование в C# 5.0",Author="Дэвис А.", Price=359}
+
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
